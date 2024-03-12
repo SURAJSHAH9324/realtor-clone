8c3411e75d9d233ddce395f315a53e105201ec3f
SURAJSHAH9324 <116291648+SURAJSHAH9324@users.noreply.github.com>
2024-03-12 20:05:59 +0530

    create router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Routes,Route, Form } from "react-router-dom";
+import Home from "./pages/Home"
+import ForgotPassword from "./pages/ForgotPassword"
+import Offers from "./pages/Offers"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
+
 function App() {
   return (
    <>
-   <h1 className='text-2xl'>Hello World</h1>
+   <Router>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/forgot-password" element={<ForgotPassword/>}/>
+      <Route path="/offer" element={<Offers/>}/>
+      <Route path="/sign-in" element={<SignIn/>}/>
+      <Route path="/sign-up" element={<SignUp/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+    </Routes>
+   </Router>
    </>
   );
 }
