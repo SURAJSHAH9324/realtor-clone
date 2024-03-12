b66670c44e0199e0db4d241c25015c22e1035e7c
SURAJSHAH9324 <116291648+SURAJSHAH9324@users.noreply.github.com>
2024-03-12 21:07:06 +0530

    make Header of our clone and make some changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import { BrowserRouter as Router, Routes,Route, Form } from "react-router-dom";
-import Home from "./pages/Home"
-import ForgotPassword from "./pages/ForgotPassword"
-import Offers from "./pages/Offers"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import Profile from "./pages/Profile"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Header from "./component/Header";
 
 function App() {
   return (
-   <>
-   <Router>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/forgot-password" element={<ForgotPassword/>}/>
-      <Route path="/offer" element={<Offers/>}/>
-      <Route path="/sign-in" element={<SignIn/>}/>
-      <Route path="/sign-up" element={<SignUp/>}/>
-      <Route path="/profile" element={<Profile/>}/>
-    </Routes>
-   </Router>
-   </>
+    <>
+      
+        <Router>
+          <Header/>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/offer" element={<Offers />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </Router>
+      
+    </>
   );
 }
 
